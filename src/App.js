b7221b4f46b19c766d7ae0f35886d0a262b613fe
rock's ezweb <alikerock@gmail.com>
2023-11-09 00:21:05 +0900

    app.js 변수 수정

--- src/App.js
+++ src/App.js
@@ -11,16 +11,16 @@ const App = ()=>{
   let max_menu_id = 3;
   const [mode, setMode] = useState('welcome');
   const [selected_id, setSelectedId] = useState(2);
-  const [subject, setSubject] = useState({
+  const subject= {
     title:'프론트엔드 개발자',
     desc:"기본언어인 html, css, javascript부터 학습합니다."
-  });
+  };
   const[menus, setMenus] = useState([
     {id:1, title:'UI/UX 개발', desc:'사용자 인터페이스와 사용자가 웹사이트를 이용하면 느끼고 생각하는 총체적 경험을 개발 '},
     {id:2, title:'재사용이 가능한 UI 개발', desc:'앵귤러, 리엑트, 뷰등의 자바스크립트 프레임워크를 가지고 재사용할 수 있는 UI를 만든다. '},
     {id:3, title:'애니메이션 구현', desc:'CSS 또는 javascript를 사용해 다양한 효과의 애니메이션 구현한다. '}
   ]);
-  const[welcome, setWelcome] = useState({title:'Welcome', desc:"Welcome to FrontEnd"});
+  const welcome = {title:'Welcome', desc:"Welcome to FrontEnd"};
 
   const getReadArticle = ()=>{
     const idx = menus.findIndex(item=>(item.id === selected_id));
