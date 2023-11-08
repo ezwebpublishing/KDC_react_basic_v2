18580999fbb4128ab7eb543ff9cc613630722d73
rock's ezweb <alikerock@gmail.com>
2023-11-08 22:34:24 +0900

    mynav 함수형 update

--- src/component/Mynav.js
+++ src/component/Mynav.js
@@ -1,42 +1,32 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
+const Mynav = ({data, onChangePage})=>{
 
-class Mynav extends Component{
-  shouldComponentUpdate(newProps, newState){
-    console.log(
-      'shouldComponentUpdate 작동',
-      newProps.data,
-      this.props.data
-      );
-      if(this.props.data === newProps.data){
-        return false;
-      }
-    return true;
-  }
-
-
-
-  render(){
-    console.log('Mynav render');
-    var lists = [];
-    var data = this.props.data;
-
+  const [list, setList] = useState([]);
+  var lists = [];
+  const getList = ()=>{    
     data.forEach((item,idx)=>{
       lists.push(<li key={item.id}><a href="/" onClick={
         (e)=>{
           e.preventDefault();
-          this.props.onChangePage(item.id);
+          onChangePage(item.id);
         }
       }>{item.title}</a></li>);
      });
-
-    return(
-      <nav>
-        <ul>
-            {lists}
-        </ul>
-      </nav>
-    );
+     setList(lists);
   }
+ 
+  useEffect(() => {        
+    getList();
+  }, [data]);  
+
+
+  return(
+    <nav>
+      <ul>
+        {list}
+      </ul>
+    </nav>
+  );
 }
 export default Mynav;
